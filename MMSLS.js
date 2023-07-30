33cbcbfaa6beb5afeafa6058db35e1b14faa10ee
3537so <87721941+3537so@users.noreply.github.com>
2023-07-31 03:14:10 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,16 +20,16 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "",
-    "",
-    "",
+    https://www.maimemo.com/share/page?uid=27304477&pid=89b9d20c4cb4eddd45fa3385d19b87c7&tid=259d58d950ce1f36c85891f37a3e0c89，
+    https://www.maimemo.com/share/page?uid=27304477&pid=89b9d20c4cb4eddd45fa3385d19b87c7&tid=259d58d950ce1f36c85891f37a3e0c89，
+    https://www.maimemo.com/share/page?uid=27304477&pid=89b9d20c4cb4eddd45fa3385d19b87c7&tid=259d58d950ce1f36c85891f37a3e0c89，
   ]
   /**
    * 生成随机数字
    * @param {number} min 最小值（包含）
    * @param {number} max 最大值（不包含）
    */
-  function randomNumber(min = 0, max = 100) {
+  function randomNumber(min = 0， max = 100) {
     return Math.min(Math.floor(min + Math.random() * (max - min)), max);
   }
   const MMSL_random = MMSLS[randomNumber(0, MMSLS.length)];
