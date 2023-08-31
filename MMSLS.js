38500fbd3b1436733b2193c963246dc4f960245f
3537so <87721941+3537so@users.noreply.github.com>
2023-09-01 03:26:28 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -6,9 +6,9 @@
 
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=15249640&pid=afaa99854200461684ec24c45b6aa6e4&tid=57653da5c7394f126d7923c9044b4dc1",
-    "https://www.maimemo.com/share/page?uid=27304477&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=31ed0620f1b6787ea55d0bf0324b8f3c",
-    "https://www.maimemo.com/share/page?uid=27304477&pid=c5ddaa52663cb88bf3920b964ff2b807&tid=31ed0620f1b6787ea55d0bf0324b8f3c",
+    "https://www.maimemo.com/share/page?uid=27304477&pid=5e2126433c9c482d59100a247837acbe&tid=95300acca4265f902535d32735f0bbcd",
+    "https://www.maimemo.com/share/page?uid=27304477&pid=5e2126433c9c482d59100a247837acbe&tid=95300acca4265f902535d32735f0bbcd",
+    "https://www.maimemo.com/share/page?uid=27304477&pid=5e2126433c9c482d59100a247837acbe&tid=95300acca4265f902535d32735f0bbcd",
   ]
 =====================以上为范例！=====================
 
