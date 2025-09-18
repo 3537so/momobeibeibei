24429fbf449739d870c8e48a4060651198b5ac18
3537so <87721941+3537so@users.noreply.github.com>
2025-09-19 04:08:26 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -22,7 +22,7 @@ const MMSLS = [
     
     https://www.maimemo.com/page?sid=30cd193b8d3781b62e052612d20b56b9&uid=27304477&pid=dd87891d3221811d53bdc40512927e93&tid=54a1738fe20a61b3e51adc642c2e45d1，
     https://www.maimemo.com/page?sid=d8444ef1dd32aa67024b840e4a381f6b&uid=27304477&pid=70da6132044e09f3b407f69d0ba23438&tid=cf462b53624a46f86f61c0819a4bf6f0，
-    https://www.maimemo.com/page?sid=aabc54e9c5dd16a004805a43e1742bda&uid=27304477&pid=9a473bcb098d3553d36e99da27676a35&tid=3e45573f58c4c434c54abf03c3d9ee42，
+    https://www.maimemo.com/page?sid=3b4bbc94b75b079d7e025e2a89ffad9c&uid=27304477&pid=e0d411ded71f3bceb6a1dfc9f9eec335&tid=ff3a8a36da11990a97beeb90b144829d，
   ]
   /**
    * 生成随机数字
