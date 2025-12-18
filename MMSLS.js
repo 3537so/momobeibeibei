3c9d2ef427137fdb51f9e2185940095e406a672b
3537so <87721941+3537so@users.noreply.github.com>
2025-12-18 14:08:31 +0800

    更新 MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    https://www.maimemo.com/share/page?uid=27304477&pid=e2c22f00bf9d79782eff5e1002d5a72d&tid=e6c67879e17def7b860dfd30f98602c8，
+    https://www.maimemo.com/share/page?uid=27304477&pid=60ff4ec509a059ea28eafa72045b10e8&tid=0f1b714e2f048d31af7f0fb6f23eadd7，
     https://www.maimemo.com/share/page?qq_aio_chat_type=3&uid=27304477&pid=0bc3db371fe1a48b6a0a7f80e3de61ca&tid=0a5ac0a22a6f07eb8f534d1276b28807，
     https://www.maimemo.com/share/page?pid=e8e899aa987ff440a5e46f0a212a544e&uid=27304477&qq_aio_chat_type=3&tid=77f425c81002bd6743bf8343a20fb387，
   ]
